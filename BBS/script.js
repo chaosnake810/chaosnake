5ab928099084527ace165ba5e3f99316cb1c5877
chaosnake810 <chaosnake810@gmail.com>
2025-11-24 02:31:28 +0900

    Add files via upload

---
+++ BBS/script.js
@@ -0,0 +1,6 @@
+window.onload = function () => {
+let xhr = new XMLHttpRequest();
+xhr.open("POST","https://script.google.com/macros/s/AKfycbwCqQ9AVZEBsCsCr_WpfNwYmOrB_7mjzaA64rR7FrlS48PSJ86c_tN_IKTJVo1fYSa7MA/exec",false);
+xhr.send();
+document.write(xhr.responseText);
+} 
