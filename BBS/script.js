162288ff46140c2e87a7e317779928817281b3e3
chaosnake810 <chaosnake810@gmail.com>
2025-11-24 03:01:21 +0900

    Update script.js

--- BBS/script.js
+++ BBS/script.js
@@ -1,10 +1,8 @@
 function loading(){
 let xhr = new XMLHttpRequest();
-xhr.open("POST","https://script.google.com/macros/s/AKfycbwCqQ9AVZEBsCsCr_WpfNwYmOrB_7mjzaA64rR7FrlS48PSJ86c_tN_IKTJVo1fYSa7MA/exec",false);
+xhr.open("POST","https://script.google.com/macros/s/AKfycbwCqQ9AVZEBsCsCr_WpfNwYmOrB_7mjzaA64rR7FrlS48PSJ86c_tN_IKTJVo1fYSa7MA/exec",true);
+xhr.onload = function() {
+  document.write(xhr.responseText);
+}
 xhr.send();
-document.write(xhr.responseText);
 } 
-
-
-
-
