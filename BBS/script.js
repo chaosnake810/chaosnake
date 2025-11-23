a632eff9658071cd213077c858910ec90732b05f
chaosnake810 <chaosnake810@gmail.com>
2025-11-24 04:16:52 +0900

    Update script.js

--- BBS/script.js
+++ BBS/script.js
@@ -1,4 +1,5 @@
 function loading(){
+alert("開始");
 let url = new URL(window.location.href);
 let params = url.searchParams;
 let page = params.get('page');
@@ -7,6 +8,9 @@ let xhr = new XMLHttpRequest();
 xhr.open("POST","https://script.google.com/macros/s/AKfycbwCqQ9AVZEBsCsCr_WpfNwYmOrB_7mjzaA64rR7FrlS48PSJ86c_tN_IKTJVo1fYSa7MA/exec",true);
 xhr.onload = function() {
 bodyElement.innerHTML = xhr.responseText;
+}
+xhr.onerror = function(){
+bodyElement.innerHTML = "エラー:" + xhr.responseText;
 }
 xhr.setRequestHeader('Content-Type', 'application/json');
 let param = {};
@@ -22,5 +26,7 @@ switch(page){
     }
 }
 xhr.send(JSON.stringify(param));
+alert("終了");
 } 
+
 
