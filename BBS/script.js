b278cdf79a4dd792f829196d17046adeeb5701a6
chaosnake810 <chaosnake810@gmail.com>
2025-11-24 02:33:44 +0900

    Update script.js

--- BBS/script.js
+++ BBS/script.js
@@ -2,5 +2,5 @@ window.onload = function () => {
 let xhr = new XMLHttpRequest();
 xhr.open("POST","https://script.google.com/macros/s/AKfycbwCqQ9AVZEBsCsCr_WpfNwYmOrB_7mjzaA64rR7FrlS48PSJ86c_tN_IKTJVo1fYSa7MA/exec",false);
 xhr.send();
-document.write(xhr.responseText);
+alert(xhr.responseText);
 } 
