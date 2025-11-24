bf051c0606e38ab768cb5b955c5a16f70cac5ad5
chaosnake810 <chaosnake810@gmail.com>
2025-11-24 18:32:28 +0900

    Update script.js

--- BBS/script.js
+++ BBS/script.js
@@ -6,7 +6,7 @@ let bodyElement = document.getElementsByTagName("body")[0];
 let xhr = new XMLHttpRequest();
 xhr.open("POST","https://script.google.com/macros/s/AKfycbwCqQ9AVZEBsCsCr_WpfNwYmOrB_7mjzaA64rR7FrlS48PSJ86c_tN_IKTJVo1fYSa7MA/exec",true);
 xhr.onload = function() {
-bodyElement.innerHTML = xhr.responseText;
+bodyElement.innerHTML = createPage(page,xhr.responseText);
 }
 xhr.onerror = function(){
 bodyElement.innerHTML = "エラー:" + xhr.responseText;
@@ -25,6 +25,28 @@ switch(page){
 }
 xhr.send(JSON.stringify(param));
 } 
+
+function createPage(page,res){
+switch(page){
+  case "thread":
+    return res;
+  default:
+    return createTopPage(res);
+}
+}
+
+function createTopPage(res){
+let threads = JSON.parse(res);
+let responseHTML = '<hr>';
+threads.map((thread)=>{
+responseHTML += '<br>'
+responseHTML += '<a href="./?page=thread＆num=' + thread.NUM + '">';
+responseHTML += thread.NUM + '.' + thread.TITLE + '(' + thread.COUNT + ')';
+responseHTML += '</a>'
+});
+responseHTML += '<br>';
+return responseHTML;
+}
 
 
 
