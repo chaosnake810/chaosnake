f02fceab9f8cfdd79c99742b0ce6200e8f4713fe
chaosnake810 <chaosnake810@gmail.com>
2025-11-25 20:57:16 +0900

    Update script.js

--- BBS/script.js
+++ BBS/script.js
@@ -87,16 +87,17 @@ return responseHTML;
 }
 
 function formatTime(time){
-const date = (time !== undifined) ? new Date(time) : new Date();
-const year = date.getFullYear();
-const month = (date.getMonth() + 1).toString().padStart(2, '0');
-const day = date.getDate().toString().padStart(2, '0');
-const hours = date.getHours().toString().padStart(2, '0');
-const minutes = date.getMinutes().toString().padStart(2, '0');
-const seconds = date.getSeconds().toString().padStart(2, '0');
+const beforeDate = ((time !== undifined) ? new Date(time) : new Date());
+const year = beforeDate.getFullYear();
+const month = (beforeDate.getMonth() + 1).toString().padStart(2, '0');
+const day = beforeDate.getDate().toString().padStart(2, '0');
+const hours = beforeDate.getHours().toString().padStart(2, '0');
+const minutes = beforeDate.getMinutes().toString().padStart(2, '0');
+const seconds = beforeDate.getSeconds().toString().padStart(2, '0');
 
 return (year + "/" + month + "/" + day + " " + hours + ":" + minutes + ":" + seconds);
 }
+
 
 
 
