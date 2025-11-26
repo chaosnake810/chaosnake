42d4dfbffbe2c16417df90db56b479560b837785
chaosnake810 <chaosnake810@gmail.com>
2025-11-26 22:08:18 +0900

    Update script.js

--- BBS/script.js
+++ BBS/script.js
@@ -156,9 +156,11 @@ return fetch('https://script.google.com/macros/s/AKfycbwCqQ9AVZEBsCsCr_WpfNwYmOr
 method: 'POST',
 body: JSON.stringify(param)
 }).then((responseText) => {
+alert(responseText);
 return responseText;
 })
 }
+
 
 
 
