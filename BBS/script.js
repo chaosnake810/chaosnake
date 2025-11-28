1d9ffa94d63b890dc6b098cc0e917128cdfa7234
chaosnake810 <chaosnake810@gmail.com>
2025-11-28 20:49:35 +0900

    Update script.js

--- BBS/script.js
+++ BBS/script.js
@@ -115,7 +115,9 @@ timeElement.className = "time";
 divElement.appendChild(timeElement);
 let messageElement = document.createElement('div');
 messageElement.className = "message";
-messageElement.innerHTML = (response.MESSAGE).replaceAll('\n','<br>');
+messageElement.innerHTML = (response.MESSAGE).replaceAll('\n','<br>').replaceAll('/![0-9,-]*/gu',((match) => {
+  return convertAnchorLink(match);
+}));
 divElement.appendChild(messageElement);
 responseHTML.appendChild(divElement);
 });
@@ -328,6 +330,7 @@ if(checkHash(str) === false){
   return str;
 }
 }
+
 
 
 
