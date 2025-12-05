27d9abad4c5cd973447a3ddc7e3351b7b6c0c319
chaosnake810 <chaosnake810@gmail.com>
2025-12-05 23:22:14 +0900

    Update script.js

--- BBS/script.js
+++ BBS/script.js
@@ -682,7 +682,6 @@ body: JSON.stringify(param)
 }
 
 function createError(error){
-try{
 if(pop.style.display === "none" || pop.style.display === undefined){
 bbs.innerHTML = "";
 let hrElement1 = document.createElement('hr');
@@ -694,15 +693,12 @@ bbs.appendChild(errorTextElement);
 let hrElement2 = document.createElement('hr');
 bbs.appendChild(hrElement2);
 }else{
-if(popError === undefined){
+if(document.getElementById('popError') === undefined){
   let errorTextElement = document.createElement('p');
   errorTextElement.id = "popError";
   popc.appendChild(errorTextElement);
 }
 popError.innerText = "Error:" + error;
-}
-}catch(e){
-  alert(e);
 }
 }
 
@@ -764,6 +760,7 @@ if(checkHash(strNumPart.split(",")) === false){
   return str;
 }
 }
+
 
 
 
