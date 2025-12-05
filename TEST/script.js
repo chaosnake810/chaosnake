384308110a5557a09a4a9ea1880128684f9f900c
chaosnake810 <chaosnake810@gmail.com>
2025-12-06 08:45:16 +0900

    Create script.js

---
+++ TEST/script.js
@@ -0,0 +1,20 @@
+function requestGAS(){
+requestButton.disabled = true;
+responseArea.innerText = "通信中...";
+let requestTime = String(new Date());
+let responseTime;
+let fetchUrl = 'https://script.google.com/macros/s/AKfycbw4vQEha63oNlJzkNkgU3QmDK4PWqNqNRA9MXAeUAPJEKPlR5s2nL4sSOS5pvUX4ZaOTQ/exec';
+return fetch(fetchUrl, {
+method: 'POST'
+}).then((response) => {
+  responseTime = String(new Date());
+  return response.text();
+}).then((response) => {
+  responseArea.innerText = "FETCH START:" + requestTime;
+  responseArea.innerText += "\n";
+  responseArea.innerText += "→GAS:" + response;
+  responseArea.innerText += "\n";
+  responseArea.innerText += "FETCH END:" + responseTime;
+  requestButton.disabled = false;
+})
+}
