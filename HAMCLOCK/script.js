a93a42c6b235d1851e19b98a5a8821207584721e
chaosnake810 <chaosnake810@gmail.com>
2026-03-10 12:29:36 +0900

    Update script.js

--- HAMCLOCK/script.js
+++ HAMCLOCK/script.js
@@ -18,19 +18,25 @@ function createClockH(){
 function formatTime(baseTime){
   const now = new Date();
   let yearDiff = now.getFullYear() - baseTime.getFullYear();
-  let monthDiff = now.getMonth() - baseTime.getMonth();
+  let monthDiff = (now.getMonth() + 1) - (baseTime.getMonth() + 1);
   if(monthDiff < 0){
     yearDiff -= 1;
-    monthDiff = monthDiff + 12;
+    monthDiff += 12;
   }
+  let dayBaseTime;
   let dayDiff = now.getDay() - baseTime.getDay();
   if(dayDiff < 0){
-    monthDiff -= 1;
-    dayDiff = Math.abs(dayDiff);
-    if(monthDiff < 0){
-      yearDiff -= 1;
-      monthDiff = monthDiff + 12;
+    if(now.getMonth() === 0)
+      dayBaseTime = new Date((now.getFullYear() - 1) + "/12/" + baseTime.getDay());
+    }else{
+      dayBaseTime = new Date(now.getFullYear() + "/" + now.getMonth() + "/" + baseTime.getDay());
     }
+  monthDiff -= 1;
+  }else{
+    dayBaseTime = new Date(now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + baseTime.getDay());
   }
+const nowDay = new Date(now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDay());
+dayDiff = (nowDay - dayBaseTime) / (1000 * 60 * 60 * 24);
+  
   return yearDiff + "年" + monthDiff + "ヶ月" + dayDiff + "日経過";
 }
