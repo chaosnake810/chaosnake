f312c329eaaafe7de146b9256a1cc6f888433777
chaosnake810 <chaosnake810@gmail.com>
2026-03-10 11:14:39 +0900

    Update script.js

--- HAMCLOCK/script.js
+++ HAMCLOCK/script.js
@@ -2,4 +2,25 @@ const mibaNeoCreatedDay = new Date("2017/09/01 00:00:00");
 const hamlanChangedDay = new Date("2018/05/24 00:00:00");
 let intervalM;
 let intervalH;
-alert(0);
+window.addEventListener("load",() => {
+  try{
+  alert(1);
+  createClock();
+  alert(2);
+  }catch(e){
+    alert(e);
+  }
+});
+function createClock(){
+  intervalM = setInterval('createClockM',1000);
+  intervalH = setInterval('createClockH',1000);
+}
+function createClockM{
+  const now = new Date();
+  mibaNeo_clock.innerText = (now - mibaNeoCreatedDay);
+}
+function createClockH{
+  const now = new Date();
+  hamlan_clock.innerText = (now - hamlanChangedDay);
+}
+
