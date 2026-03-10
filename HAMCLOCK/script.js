924b5f131ad79907fbd1ee6d60642e2f15f3f4a8
chaosnake810 <chaosnake810@gmail.com>
2026-03-10 11:08:06 +0900

    Add alert for debugging on load event

--- HAMCLOCK/script.js
+++ HAMCLOCK/script.js
@@ -2,6 +2,7 @@ const mibaNeoCreatedDay = new Date("2017/09/01 00:00:00");
 const hamlanChangedDay = new Date("2018/05/24 00:00:00");
 let intervalM;
 let intervalH;
+alert(0);
 window.addEventListener("load", (event) => {
   try{
   alert(1);
