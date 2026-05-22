76ae104bc322349a26ae5f21261c4e7f9956c84d
chaosnake810 <chaosnake810@gmail.com>
2026-05-23 02:34:56 +0900

    Update script.js

--- HAMCLOCK/script.js
+++ HAMCLOCK/script.js
@@ -15,13 +15,31 @@ function createClockM(){
   const texts = formatTime(mibaNeoCreatedDay);
   mibaNeo_clock_diff.innerText = texts[0];
   mibaNeoAnniversary.innerText = texts[1];
+  if(texts[2] === null){
+    mibaNeo_clock_anniversary.style.display = "none";
+    mibaNeo_aniversary_text_before.style.display = "block";
+    mibaNeo_aniversary_text_after.innerText = "！！";
+  } else {
+  mibaNeo_aniversary_text_before.style.display = "none";
+  mibaNeo_clock_anniversary.style.display = "block";
   mibaNeo_clock_anniversary.innerText = texts[2];
+  mibaNeo_aniversary_text_after.innerText = "まであと";
+  }
 }
 function createClockH(){
   const texts = formatTime(hamlanChangedDay);
   hamlan_clock_diff.innerText = texts[0];
   hamlanAnniversary.innerText = texts[1];
+  if(texts[2] === null){
+    hamlan_clock_anniversary.style.display = "none";
+    hamlan_aniversary_text_before.style.display = "block";
+    hamlan_aniversary_text_after.innerText = "！";
+  } else {
+  hamlan_aniversary_text_before.style.display = "none";
+  hamlan_clock_anniversary.style.display = "block";
   hamlan_clock_anniversary.innerText = texts[2];
+  hamlan_aniversary_text_after.innerText = "まであと";
+  }
 }
 function formatTime(baseTime){
   const now = new Date();
@@ -46,6 +64,9 @@ function formatTime(baseTime){
 const nowDay = new Date(now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate());
 dayDiff = (nowDay - dayBaseTime) / (1000 * 60 * 60 * 24);
   const diffText = yearDiff + " 年 " + monthDiff + " ヶ月 " + dayDiff + " 日経過";
+  if((baseTime.getMonth() === now.getMonth()) && (baseTime.getDate() === now.getDate())){
+    return [diffText,yearDiff,null];
+  }
   const anniversary = yearDiff + 1;
   let anniversaryDay = new Date(now.getFullYear() + "/" + (baseTime.getMonth() + 1) + "/" + baseTime.getDate() + " 00:00:00");
   if(anniversaryDay < now){
